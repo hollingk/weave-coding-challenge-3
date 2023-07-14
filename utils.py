fd0ff39683fdb5935253aa03d81881cba17403f1
Kristy <hollingk@gmail.com>
2023-07-14 01:24:55 -0600

    functionify

--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@ from typing import Dict, List, Union, Any
 # This function is used to extract values from a (deep) nested JSON object
 # Credit: https://gist.github.com/toddbirchard/b6f86f03f6cf4fc9492ad4349ee7ff8b?permalink_comment_id=4178846#gistcomment-4178846
 def extract_values(
-    object_to_search: Union[Dict[Any, Any], List[Any]], search_key: str
+    object_to_search: Union[Dict[Any, Any], List[Any]], 
+    search_key: str, 
+    fuzzy_match: bool=False,
 ) -> List:
     """Recursively pull values of specified key from nested JSON."""
     results_array: List = []
@@ -12,6 +14,7 @@ def extract_values(
         object_to_search: Union[Dict[Any, Any], List[Any]],
         results_array: List[Any],
         search_key: str,
+        fuzzy_match: bool=False,
     ) -> List:
         """Return all matching values in an object."""
         if isinstance(object_to_search, dict):
@@ -19,15 +22,19 @@ def extract_values(
                 if isinstance(val, (dict, list)):
                     if key == search_key:
                         results_array.append(val)
-                    extract(val, results_array, search_key)
+                    elif fuzzy_match and search_key.lower() in key.lower():
+                        results_array.append(val)
+                    extract(val, results_array, search_key, fuzzy_match)
                 elif key == search_key:
+                    results_array.append(val)
+                elif fuzzy_match and search_key.lower() in key.lower():
                     results_array.append(val)
         elif isinstance(object_to_search, list):
             for item in object_to_search:
-                extract(item, results_array, search_key)
+                extract(item, results_array, search_key, fuzzy_match)
         return results_array
 
-    results = extract(object_to_search, results_array, search_key)
+    results = extract(object_to_search, results_array, search_key, fuzzy_match)
     to_return = []
     for result in results:
         if type(result) == list:
